2f3c2af4ca31818e782fb506270a9fcc12b706c8
Johan Baez <ancura@Johans-MacBook-Pro.local>
2016-06-17 14:18:13 -0400

    second js challenge

---
+++ Johan-challenge-2.js
@@ -0,0 +1,21 @@
+console.clear();
+function player(name) {
+  var timePlayed;
+  jugador = name;
+  if (jugador == "shaw") {
+    timePlayed = " 1 year.";
+    console.log(jugador + " is a great player for the Red Sox. He has been on the team for" + timePlayed);
+  }
+  
+  else if (jugador == "pedroia") {
+    timePlayed = " 12 years.";
+    console.log(jugador + " is a great player for the Red Sox. He has been on the team for" + timePlayed);
+  }
+  
+  else jugador == "betts"
+    timePlayed = " 2 years.";
+    console.log(jugador + " is a great player for the Red Sox. He has been on the team for" + timePlayed);
+}
+//
+
+player("pedroia");
